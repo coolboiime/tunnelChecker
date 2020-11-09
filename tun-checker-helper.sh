31197056be27bb5fcedbb0e1deced3d4fdaad81a
Jacky Liang <coolboii@me.com>
2020-11-08 22:51:19 -0500

    Renamed project

--- tun-checker-helper.sh
+++ tun-checker-helper.sh
@@ -2,7 +2,7 @@
 CONTAINER_NAME=transmission-openvpn
 TUN_CHECKER=/data/transmission-home/tun-checker.sh
 
-# Run tun_checker script
+# Run tun-checker script
 docker exec $CONTAINER_NAME bash $TUN_CHECKER
 
 exit 0
